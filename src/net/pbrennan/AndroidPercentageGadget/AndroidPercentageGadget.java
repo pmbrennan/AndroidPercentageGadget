0e35636a21b8dbfbf71b0daac1812f51ebd4f6eb
Patrick M Brennan <pbrennan@gmail.com>
2014-03-29 15:04:51 -0700

    initial commit

---
+++ src/net/pbrennan/AndroidPercentageGadget/AndroidPercentageGadget.java
@@ -0,0 +1,15 @@
+package net.pbrennan.AndroidPercentageGadget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidPercentageGadget extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
